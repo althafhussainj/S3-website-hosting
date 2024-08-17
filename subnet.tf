8addcca48a2094c4e4cc09f1747fd24950077178
shabana syed <althafhussainj@gmail.com>
2024-08-17 13:27:02 +0530

    az edited

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.cidr[count.index]
-#  availability_zone = var.az[count.index]
-  availability_zone = var.az
+  availability_zone = var.az[count.index]
+#  availability_zone = var.az
   count = 2
 
   tags = {
